637c33304b6fe518e57f9fa4e3b7a65b8296f9b8
kordano <konrad.kuehne@rocketmail.com>
2015-04-07 22:52:57 +0200

    rotating cube

--- src/cljs/cezanne/core.cljs
+++ src/cljs/cezanne/core.cljs
@@ -4,6 +4,10 @@
 
 (.log js/console "Kneel before the holy Kordano!")
 
+
+(def state (atom {}))
+
+
 (defn init-all []
   (let [scene (js/THREE.Scene.)
         camera (js/THREE.PerspectiveCamera.
@@ -13,17 +17,27 @@
                     1000)
         renderer (js/THREE.WebGLRenderer.)
         geometry (js/THREE.BoxGeometry. 1 1 1)
-        material (js/THREE.MeshBasicMaterial. {"color" "0x00ff00"})
+        material (js/THREE.MeshBasicMaterial. (clj->js {:color 0x5555ff}))
         cube (js/THREE.Mesh. geometry material)]
     (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
     (.. js/document -body (appendChild (.-domElement renderer)))
     (.add scene cube)
     (set! (.. camera -position -z) 5)
-    {:scene scene
-     :camera camera
-     :renderer renderer}))
+    (reset! state
+          {:scene scene
+           :camera camera
+           :cube cube
+           :renderer renderer})))
 
 
-(defn render [{:keys [scene camera renderer] :as state}]
-  (js/requestAnimationFrame render state)
-  (.render renderer scene camera))
+
+(defn render []
+  (js/requestAnimationFrame render)
+  (.render (:renderer @state) (:scene @state) (:camera @state))
+  (set! (.. (:cube @state) -rotation -x) (+ (.. (:cube @state) -rotation -x) 0.1))
+  (set! (.. (:cube @state) -rotation -y) (+ (.. (:cube @state) -rotation -y) 0.1))
+  )
+
+
+(init-all)
+(render)
